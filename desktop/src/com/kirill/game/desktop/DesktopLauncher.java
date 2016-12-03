197477799afaaa37e19d7389a8a30744a8c62289
Kirill <mazkirand@gmail.com>
2016-12-03 18:15:16 +0300

    initial commit

---
+++ desktop/src/com/kirill/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kirill.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kirill.game.WallsBall;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WallsBall(), config);
+	}
+}
